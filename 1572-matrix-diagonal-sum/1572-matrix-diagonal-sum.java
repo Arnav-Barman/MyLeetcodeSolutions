32729781bbb75e7f64173f56347f201b4f3c99b2
Arnav Barman <86536812+Arnav-Barman@users.noreply.github.com>
2022-04-19 00:33:23 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (81.97%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -2,12 +2,15 @@ class Solution {
     public int diagonalSum(int[][] mat) {
         int n = mat.length;
         int sum=0;
-        for(int i =0; i<n; i++){
-            for(int j=0;j<n;j++){
-                if((i+j) == (n-1) || i==j)
-                    sum += mat[i][j];
-            }
+        int i=0, j=0;
+        while(i<n && j<n){
+            sum += mat[i][j];
+            sum += mat[i][n-1-j];
+            i++; 
+            j++;
         }
+        if(n%2 != 0 )
+            sum -= mat[n/2][n/2];
         return sum;
     }
 }
